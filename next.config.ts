6bbfd80869b1d3658d75b2bc1a849df00aaf0f74
Mykhaylo Zelisko <zelisko.mykhaylo@gmail.com>
2025-06-19 13:04:48 +0300

    fix build

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  sassOptions: {
-    implementation: 'sass-embedded',
-  },
+  // sassOptions: {
+  //   implementation: 'sass-embedded',
+  // },
   turbopack: {
     rules: {
       '*.svg': {
