a07f780f7d5c051fe09b4b644af7549daf813354
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2025-07-24 22:20:24 +0200

    change file mane

--- src/app/components/HomePage/YourMoment/YourMoment.jsx
+++ src/app/components/HomePage/YourMoment/YourMoment.jsx
@@ -1,5 +1,5 @@
 import stylesShared from '@/app/components/HomePage/homePage.module.scss';
-import styles from '@/app/components/HomePage/YourMoment/yourMoment.module.scss';
+import styles from '@/app/components/HomePage/YourMoment/YourMoment.module.scss';
 
 export default function YourMoment() {
   return (
